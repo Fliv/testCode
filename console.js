893fe63bc5ae6f301a896c9ab50b448e8bfc4ed8
Fliv <415071280@qq.com>
2026-05-12 20:38:06 +0800

    Modify console.log to include 'html' in output
    ```

--- console.js
+++ console.js
@@ -1,3 +1,3 @@
-console.log(String.fromCharCode(96).repeat(3));
+console.log(String.fromCharCode(96).repeat(3)+'html'+'\n');
 console.log('\n');
 console.log('![test](https://svg.pp2cat.uk/svg/a.svg)')
