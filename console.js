b36f8fa7d97bf05b82350da296d208af5d3704ac
Fliv <415071280@qq.com>
2026-05-12 20:42:11 +0800

    Remove duplicate console.log statements

--- console.js
+++ console.js
@@ -1,3 +1,2 @@
-console.log(String.fromCharCode(96).repeat(3)+'html'+'\n');
-console.log('\n');
+console.log(String.fromCharCode(96).repeat(3)+'\n');
 console.log('![test](https://svg.pp2cat.uk/svg/a.svg)')
